75663f29f3b134a3bd2620efb9b7a674b6236e09
lwkai <1275124829@163.com>
2026-04-24 03:08:58 +0000

    Deploying to gh-pages from @ lwkai/docs@968ffed9ea9c9893e21b77956706d9e141b52660 🚀

---
+++ docs__VUE__element.md.42f93241.async.js
@@ -0,0 +1,23 @@
+"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[641],{20658:function(o,d,_){_.r(d);var s=_(82532),c=_(36800),r=_(31546),i=_(76607),E=_(377),l=_(91769),h=_(45122),b=_(50760),t=_(92358),u=_(11872),m=_(75271),n=_(9024),e=_(52676);function a(){return(0,e.jsx)(t.dY,{children:(0,e.jsx)(m.Suspense,{fallback:(0,e.jsx)(u.Z,{}),children:(0,e.jsx)(e.Fragment,{children:(0,e.jsxs)("div",{className:"markdown",children:[(0,e.jsxs)("h2",{id:"\u8986\u76D6\u997F\u4E86\u4E48\u7EC4\u4EF6\u7684\u6837\u5F0F",children:[(0,e.jsx)("a",{"aria-hidden":"true",tabIndex:"-1",href:"#\u8986\u76D6\u997F\u4E86\u4E48\u7EC4\u4EF6\u7684\u6837\u5F0F",children:(0,e.jsx)("span",{className:"icon icon-link"})}),"\u8986\u76D6\u997F\u4E86\u4E48\u7EC4\u4EF6\u7684\u6837\u5F0F"]}),(0,e.jsx)("p",{children:n.texts[0].value}),(0,e.jsx)("p",{children:n.texts[1].value}),(0,e.jsx)(l.Z,{lang:"html",children:n.texts[2].value})]})})})})}d.default=a},9024:function(o,d,_){_.r(d),_.d(d,{texts:function(){return s}});const s=[{value:"\u5982\u679C\u5728\u67D0\u4E2A\u4F4D\u7F6E\u8981\u8986\u76D6\u6846\u67B6\u4E2D\u7684\u6837\u5F0F\uFF0C\u5982\u679C deep \u90FD\u4E0D\u80FD\u751F\u6548\u65F6\uFF0C\u53EF\u4EE5\u7528\u6837\u5F0F\u6A21\u5757\u7684\u65B9\u5F0F\u5904\u7406",paraId:0,tocIndex:0},{value:"\u6BD4\u5982\uFF0C\u4E0B\u9762\u8981\u8986\u76D6 dialog \u5F39\u51FA\u5BF9\u8BDD\u6846\u4E2D\u7684\u6837\u5F0F: el-dialog__body \u9700\u8981\u8FDB\u884C\u5C40\u90E8\u8986\u76D6\u3002",paraId:1,tocIndex:0},{value:`<template>
+  <el-dialog
+      class="el-dialog-full"
+      :class="$style.test0001"  <!-- \u4E3B\u8981\u8FD9\u91CC\u5F15\u7528 test0001 -->
+      title="\u9009\u62E9\u6A21\u677F"
+      v-model="dialogVisible"
+      top="10px"
+      width="90%"
+      destroy-on-close
+      @closed="isEmpty = false"
+    > 
+    ...
+</template>
+<style module lang="scss">
+.test0001 { // \u5728\u8FD9\u91CC\u5B9A\u4E49\u8FD9\u4E2A\u6837\u5F0F
+  :global { // \u6CE8\u610F\u5FC5\u987B\u52A0\u4E0A\u8FD9\u4E2A
+    .el-dialog__body { // \u4E0B\u7EA7\u9700\u8981\u8986\u76D6\u7684\u6837\u5F0F\u540D\u79F0
+      overflow: hidden !important;
+    }
+  }
+}
+</style>
+`,paraId:2,tocIndex:0}]}}]);
